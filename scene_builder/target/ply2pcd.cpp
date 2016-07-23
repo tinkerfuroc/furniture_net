b612d7bf78263b4fd8b4242de15e57e81f13ed7d
gjc13 <amtcmp@126.com>
2016-07-23 19:21:37 +0800

    [Change] Directly save from mesh file

--- scene_builder/target/ply2pcd.cpp
+++ scene_builder/target/ply2pcd.cpp
@@ -7,56 +7,38 @@
 #include <pcl/console/print.h>
 #include <pcl/console/parse.h>
 #include <pcl/console/time.h>
+#include <pcl/visualization/pcl_visualizer.h>
 
 using namespace pcl;
 using namespace pcl::io;
 using namespace pcl::console;
 
 
-int
-main (int argc, char** argv)
-{
-  bool binary = true;
+int main(int argc, char** argv) {
+    bool binary = true;
 
-  if(argc < 3) {
-    std::cerr << "Usage:" << std::endl;
-    std::cerr << argv[0] << " [-a] input.ply output.pcd" << std::endl;
-    std::cerr << "\t-a\tASCII output" << std::endl;
-    return (1);
-  }
-
-  if(argc == 4) {
-    if(strncmp(argv[1],"-a",2) != 0) {
-      std::cerr << "Error: unknown option!" << std::endl;
-      return (1);
+    if (argc < 3) {
+        std::cerr << "Usage:" << std::endl;
+        std::cerr << argv[0] << " [-a] input.ply output.pcd" << std::endl;
+        std::cerr << "\t-a\tASCII output" << std::endl;
+        return (1);
     }
-    else {
-      binary = false;
-      argv += 1;
+
+    if (argc == 4) {
+        if (strncmp(argv[1], "-a", 2) != 0) {
+            std::cerr << "Error: unknown option!" << std::endl;
+            return (1);
+        } else {
+            binary = false;
+            argv += 1;
+        }
     }
-  }
 
- 
-  
-  pcl::PolygonMesh mesh;
+    pcl::PolygonMesh mesh;
 
-  pcl::io::loadPolygonFilePLY(argv[1], mesh);
+    pcl::io::loadPolygonFile(argv[1], mesh);
+    pcl::PCDWriter pcdwriter;
+    pcdwriter.writeBinary(argv[2], mesh.cloud);
 
-  std::cerr << "Read cloud: " << std::endl;
-  pcl::io::saveVTKFile ("temp.vtk", mesh);
-
-  // then use pcl_vtk2pcd
-
-  vtkSmartPointer<vtkPolyDataReader> reader = vtkSmartPointer<vtkPolyDataReader>::New ();
-  reader->SetFileName ("temp.vtk");
-  reader->Update ();
-  vtkSmartPointer<vtkPolyData> polydata = reader->GetOutput ();
-  
-  pcl::PointCloud<pcl::PointXYZ> cloud;
-  vtkPolyDataToPointCloud (polydata, cloud);
-
-  pcl::PCDWriter pcdwriter;
-  pcdwriter.write<pcl::PointXYZ> (argv[2], cloud);
-
-  return (0);
+    return (0);
 }
